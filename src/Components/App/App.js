dbfaf457074912aff92ae1652684577bf1776625
Mike Murphy <murph517@gmail.com>
2021-07-30 13:26:03 -0700

    Updates App, Playlist, SearchBar Components to step 76. Creates Spotify module

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -43,6 +43,9 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -58,6 +61,17 @@ class App extends React.Component {
       let tracks =this.state.playlistTracks;
       tracks.filter(track => track.id);
     }
+    updatePlaylistName(name){
+      this.setState({
+        playlistName : name
+      });
+    }
+    savePlaylist() {
+      const arrURI = this.state.playlistTracks.map( playlistTrack => playlistTrack.uri)
+    }
+    search(term) {
+      console.log(term);
+    }
 
   render() {
     return (
@@ -65,10 +79,10 @@ class App extends React.Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-            <Playlist searchResults={this.state.searchResults} playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove = {this.state.removeTrack}/>
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist searchResults={this.state.searchResults} playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove= {this.removeTrack} onNameChange={this.updatePlaylistName} onSave= {this.savePlaylist}/>
           </div>
         </div>
       </div>
