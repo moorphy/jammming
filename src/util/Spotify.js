09bb792e9d0c739ab454f180d45d5dec18d69e2c
Mike Murphy <murph517@gmail.com>
2021-08-02 10:52:20 -0700

    Spotify.js completed to step 79

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,23 @@
 
+let accessToken;
 
 const Spotify = {
+
+    getAccessToken(){
+        if(accessToken){
+            return accessToken;
+        }
+        const hasAccessToken = window.location.href.match(/access_token=([^&]*)/);
+        const hasExpiresIn = window.location.href.match(/expires_in=([^&]*)/);
+        if (hasAccessToken && hasExpiresIn){
+            accessToken = hasAccessToken[1];
+            const expiresIn = Number(hasExpiresIn[1]);
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
+    }
+
 
 }
 export default Spotify;
